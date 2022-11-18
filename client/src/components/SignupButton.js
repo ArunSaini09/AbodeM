dd82f78a9e2806388476c3422d9e73ab0f5a4aa0
ArunSaini09 <arunsaini0918@gmail.com>
2022-11-18 10:36:58 -0500

    create SignUpButton component

---
+++ client/src/components/SignupButton.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+const SignUpButton = () => {
+  const auth = useAuth();
+  const navigate = useNavigate();
+
+  if (!auth.isAuthenticated) {
+    return (
+      <Link className="btn btn-primary" to="/signup">
+        Sign Up
+      </Link>
+    );
+  }
+  //if user is authenticated do not render the signup button
+  return (
+    <>
+    </>
+  )
+  /* const logout = () => {
+    auth.signout().then(() => navigate("/"));
+  };
+
+  return (
+    <div className="text-white">
+      Welcome! {auth.user.firstName}
+      <button className="btn btn-primary" onClick={logout}>
+        Logout
+      </button>
+    </div>
+  ); */
+};
+
+export default SignUpButton;
